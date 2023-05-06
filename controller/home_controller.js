e9199cbee72097b3d05a72bf977b4e171d45e028
kin6sman <domin12313xyz@gmail.com>
2023-05-06 12:55:17 +0530

    folder structuring

---
+++ controller/home_controller.js
@@ -0,0 +1,4 @@
+module.exports.home = (req, res) => {
+  console.log(req.cookies);
+  return res.render("home");
+};
---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+app.get("/", require("./routes"));
+
+app.listen(8000, () => {
+  console.log("server is running at port: 8000");
+});
