c75b817efdba0f94fb98f35033bedce1a91c386e
kin6sman <domin12313xyz@gmail.com>
2023-05-09 14:10:37 +0530

    added express layouts

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const express = require("express");
+const path = require("path");
+const expressLayout = require("express-ejs-layouts");
 const ejs = require("ejs");
 const db = require("./config/mongoose");
 
 const app = express();
 
 app.use(express.static("./assets"));
+app.use(express.urlencoded());
 
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
-app.get("/", require("./routes"));
+app.use(expressLayout);
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
+
+app.use("/", require("./routes/index"));
+// app.get("/", require("./routes"));
 
 app.listen(8000, () => {
   console.log("server is running at port: 8000");
